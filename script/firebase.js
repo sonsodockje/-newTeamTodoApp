6cc227b2bb559b8469bfc79929c28d63a9a98174
sonsodockje <hanaroo0115@naver.com>
2024-02-05 23:03:48 +0900

    🚧 파이어베이스 샘플 코드 보완

--- script/firebase.js
+++ script/firebase.js
@@ -4,9 +4,9 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  onAuthStateChanged,
   sendEmailVerification,
   updateProfile,
+  signOut,
 } from "firebase/auth";
 import {
   getFirestore,
@@ -28,18 +28,37 @@ const firebaseConfig = {
   measurementId: "G-KQK2SD3VXF",
 };
 
-// 파이어 베이스 초기화
 const app = initializeApp(firebaseConfig);
-// 디비 불러옴
 const db = getFirestore(app);
+getUserStateAndTodos();
+const test = document.querySelector(".test");
+const notLogin = document.querySelector(".user-wrap-logout");
 
 // 특정 "문서" 가져오는 코드
 async function getDocument(uid) {
   const docRef = doc(db, "todos", uid);
   const docSnap = await getDoc(docRef);
+  const todoWrap = document.querySelector(".todos-wrap");
 
   if (docSnap.exists()) {
-    console.log("Document data:", docSnap.data());
+    console.log("Document data:", docSnap.data().topics);
+    docSnap.data().topics.forEach((item) => {
+      {
+        const 템플릿 = `<div class="todo-card-wrap ">
+                        <div class="todo-card-header">
+                          <span class="todo-card-title">${item}</span>
+                          <button class="opened_btn">
+                            <span class="material-symbols-outlined">
+                              menu
+                            </span>
+                          </button>
+                        </div>
+                      </div>`;
+
+        todoWrap.innerHTML += 템플릿;
+      }
+    });
+
     /// 하위 컬렉션의 모든 문서 가져오기
     // 여기를 반복문으로 돌려야됨.
     const querySnapshot = await getDocs(
@@ -55,9 +74,9 @@ async function getDocument(uid) {
 
 // 로그인한 회원의 유아이디를 매개변수로 전달하면
 // 문서를 가져올때 유아이디를 검색하여 사용자의 투두만 가져오게 됨.
-getDocument("EivH5GMe2APglKbS0V1AXNRV4Kv1");
 
 // 회원가입하는 코드
+
 export function singup() {
   const auth = getAuth();
   const displayName = "홍길동3";
@@ -118,7 +137,7 @@ async function uploadUserTodo(uid) {
     test: "test",
   });
 }
-
+login();
 // 로그인 하는 코드
 export function login() {
   const auth = getAuth();
@@ -129,6 +148,9 @@ export function login() {
       // Signed in
       const user = userCredential.user;
       console.log("로그인됨 : ", user);
+      // 로컬스토리지에 user.uid 저장
+      //
+      getUserStateAndTodos();
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -139,19 +161,27 @@ export function login() {
 
 // 로그인 후 사용자 데이터 가져오기(현재 로그인한 사용자 가져오기)
 // 로그 아웃 전까지 유지? 되는? 듯 하는? 그런? 거?
-export function userState() {
+
+export function getUserStateAndTodos() {
   const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      console.log("로그인 중인 상태 : ", user);
-    } else {
-      // User is signed out
-      // ...
-      console.log("로그인 필요한 상태");
-    }
-  });
+  const user = auth.currentUser;
+
+  if (user) {
+    const uid = user.uid;
+    console.log("로그인됨 ", user);
+
+    // - [O] 디폴트 투두 지우기
+    // - [O] 로그인창 지우기
+    test.style.display = "none";
+    notLogin.style.display = "none";
+
+    // - [O] 사용자 디비 불러옴.
+    getDocument(uid);
+
+    // - [ ] 우측 상단아이콘 띄우기.
+  } else {
+    // - [ ] 로그인, 회원가입 오른쪽편 띄움.
+  }
 }
 
 // 이메일 인증 메일 보내기
@@ -164,16 +194,13 @@ export function emailVerification() {
   });
 }
 
-// 로그아웃
-// export function logout() {
-//   const onSignOut = async () => {
-//     try {
-//       const auth = getAuth(app);
-//       await signOut(auth);
-//       toast.success("로그아웃 되었습니다.");
-//     } catch (error: any) {
-//       console.log(error);
-//       toast.error(error?.code);
-//     }
-//   };
-// }
+function logout() {
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      console.log("로그웃완료");
+    }) // logout successful
+    .catch((error) => {
+      console.log(error);
+    }); // logout fail
+}
